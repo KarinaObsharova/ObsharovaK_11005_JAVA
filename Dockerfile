28fba55ca022906b47927b5ffcdfd959f148ab4f
“KarinaObsharova” <obsharovaa@gmail.com>
2021-04-23 00:27:08 +0300

    TelegramBot

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:latest
-ADD target/application-jar-with-dependencies.jar ./
-CMD ["java", "-jar", "application-jar-with-dependencies.jar"]
-
--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-public class Main {
-    public static void main(String[] args) throws TelegramApiException {
-        Bot bot = new Bot();
-        bot.run();
-    }
-}

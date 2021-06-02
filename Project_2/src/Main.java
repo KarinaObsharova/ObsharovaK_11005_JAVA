155e136ca4debd159b95611c102f08adaf4a2b81
“KarinaObsharova” <obsharovaa@gmail.com>
2021-06-02 17:08:31 +0300

    controlWork_02.06

---
+++ Project_2/src/Main.java
@@ -0,0 +1,73 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+            Scanner sc = new Scanner(System.in);
+            int x = sc.nextInt();
+            switch (x) {
+                case 0 -> System.exit(0);
+                case 1 -> new Thread(Main::task1).start();
+                case 2 -> new Thread(Main::task2).start();
+                case 3 -> new Thread(Main::task3).start();
+            }
+
+    }
+    static void task1(){
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File("./src/task1.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        var map = sc != null ? sc.tokens()
+                .map(x -> x.split("[|]"))
+                .collect(Collectors.groupingBy(x -> x[0]))
+                .entrySet().stream().collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        x -> x.getValue().stream().collect(Collectors.groupingBy(y -> y[1]))
+                                .entrySet()
+                                .stream()
+                                .map(n -> n.getKey() + " " + n.getValue().stream()
+                                        .map(y -> Integer.parseInt(y[2]))
+                                        .reduce(Integer::sum).get().toString())
+                                .collect(Collectors.toMap(z -> z.split(" ")[0], z -> Integer.parseInt(z.split(" ")[1])
+                                )))) : null;
+
+        map.entrySet().stream().collect(Collectors.toMap(
+                Map.Entry::getKey,
+                x->x.getValue().entrySet().stream().max(Map.Entry.comparingByValue())
+                        .stream().collect(Collectors.toMap(
+                                Map.Entry::getKey,
+                                Map.Entry::getValue)))).entrySet().forEach(System.out::println);
+    }
+    static void task2(){
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File("./src/task2.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        var map = sc.tokens().map(x -> x.split("[|]"))
+                .collect(Collectors.groupingBy(x -> x[0]))
+                .entrySet()
+                .stream()
+                .map(x -> x.getKey() + " " + x.getValue().stream()
+                        .map(y -> Integer.parseInt(y[1]))
+                        .reduce(Integer::sum).get().toString())
+                .collect(Collectors.toMap(x -> x.split(" ")[0], x -> Integer.parseInt(x.split(" ")[1])));
+        var average = map.values().stream().mapToInt(integer -> integer).average().getAsDouble();
+        map.entrySet().stream().filter(x -> x.getValue() > average).forEach(System.out::println);
+    }
+    static void task3(){
+
+    }
+}
+

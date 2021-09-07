8a8d95f111bab4cfae82deef8a698c949cddf235
“KarinaObsharova” <obsharovaa@gmail.com>
2021-09-08 00:07:32 +0300

    done

--- 01.SpringDemo/src/main/java/MainSpring.java
+++ 01.SpringDemo/src/main/java/MainSpring.java
@@ -1,32 +1,11 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.convert.Property;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Properties;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
-import javax.sql.DataSource;
 
 public class MainSpring {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
         SignUpService signUpService = context.getBean(SignUpService.class);
         signUpService.singUp("obsharova@gmail.com", "qwerty007");
-/*
-        Properties properties = new Properties();
-        properties.load(new FileReader("src//main//resources//application.properties"));*/
 
-
-     /*   HikariConfig config = new HikariConfig();
-        config.setDriverClassName(properties.getProperty("db.driver"));
-        config.setJdbcUrl(properties.getProperty("db.url"));
-        config.setUsername(properties.getProperty("db.user"));
-        config.setPassword(properties.getProperty("db.password"));
-
-        DataSource dataSource = new HikariDataSource(config);
-        PasswordBlackList passwordBlackLis = new PasswordBlackListJDBCImpl(dataSource);*/
     }
 }
--- 01.SpringDemo/src/main/java/PasswordBlackListJDBCImpl.java
+++ 01.SpringDemo/src/main/java/PasswordBlackListJDBCImpl.java
@@ -1,6 +1,9 @@
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
+import java.util.List;
+
 
 public class PasswordBlackListJDBCImpl implements PasswordBlackList{
     private JdbcTemplate jdbcTemplate;
@@ -8,13 +11,19 @@ public class PasswordBlackListJDBCImpl implements PasswordBlackList{
     public PasswordBlackListJDBCImpl(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
+
     //language=SQL
     private static final String SQL_SELECT_ALL = "select * from password_black_list where password = ?";
 
+    private final RowMapper<String> stringRowMapper = (row, rowNumber) -> {
+        String str = row.getString("password");
+        return str;
+    };
+
     @Override
     public boolean contains(String password) {
-        Integer cnt = jdbcTemplate.queryForObject(SQL_SELECT_ALL, Integer.class, password);
-        return cnt == null;
+              List<String> listPassword = jdbcTemplate.query(SQL_SELECT_ALL, stringRowMapper, password);
+        return !listPassword.isEmpty();
 
 
 
